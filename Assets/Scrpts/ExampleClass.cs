8acd7ffc50140ecd0e569c3d82178bfcc14b955d
Culver Stone <culver.stone57@gmail.com>
2024-03-10 21:29:31 -0700

    Started working on creating a particle collision detector code, as part of my overall goal of creating a game

---
+++ Assets/Scrpts/ExampleClass.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExampleClass : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        //Debug.Log("Collision Detected");
+
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Vector3 force = collisionEvents[i].velocity * 10;
+                rb.AddForce(force);
+            }
+            i++;
+        }
+    }
+}
